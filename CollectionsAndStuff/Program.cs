85bfddbb2fc2a7229e22dd80d22712662e455616
BenJarrett <b.cl.jarrett@gmail.com>
2021-08-02 18:19:09 -0500

    Add project files.

---
+++ CollectionsAndStuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectionsAndStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
